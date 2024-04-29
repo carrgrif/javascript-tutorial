15afcaed28d8dc286cbb390f62fa3f114de20c94
carrgrif <gmcarr5@gmail.com>
2024-04-28 22:49:39 -0400

    upload first 2 chapters

---
+++ chapter_2/sandbox.js
@@ -0,0 +1,19 @@
+// console.log(1);
+// console.log(2);
+
+// let and const are new ways to create variables, var is the old way
+
+let age = 25;
+let year = 2019;
+
+console.log(age, year);
+
+age = 30;
+console.log(age);
+
+const points = 100;
+// points = 50; Can't do this, it's a constant variable
+console.log(points);
+
+var score = 75;
+console.log(score);

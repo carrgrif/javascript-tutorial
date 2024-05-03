3565e0eb11095331b8f7066af0f9fcfeac7a677a
Griffin Carr <griffin@goodshuffle.com>
2024-05-03 17:31:52 -0400

    covered strings part 2 and numbers part 1

---
+++ chapter_2/numbers.js
@@ -0,0 +1,39 @@
+let radius = 10;
+const pi = 3.14;
+
+//console.log(radius, pi);
+
+// math operators +, -, *, /, ** (power of), % (remainder)
+// console.log(10 / 2);
+// let result = radius % 3;
+
+// let result = pi * radius ** 2;
+
+// order of operation - B I D M A S - Brackets, Indices, Division, Multiplcation, Addition, Subtraction
+
+// let result = 5 * (10 - 3)**2; // 10 - 3 first, then 6**2, then 36 * 5 = 245
+
+// console.log(result);
+
+let likes = 10;
+// likes = likes + 1;
+// likes++;
+// likes--;
+
+// likes += 10;
+// likes -= 5;
+// likes *= 2;
+// likes /= 2;
+
+// console.log(likes);
+
+// NaN - not a number: tried to do some sort of calc that doesn't result in a number
+
+// console.log(5 / 'hello');
+// console.log(5 * 'hello');
+
+// converts a Number to a string implicitly for variable result - gets messy when there are many variables, so use template strings instead
+let result = 'the blog has ' + likes + ' likes';
+console.log(result);
+
+// Ended at 46:24
---
+++ chapter_2/strings2.js
@@ -0,0 +1,19 @@
+// common string methods
+
+let email = 'mario@thenetninja.co.uk';
+
+// let result = email.lastIndexOf('n');
+
+// let result = email.slice(2, 5);
+
+// Deprecated, don't use this
+// let result = email.substr(4, 10);
+
+// let result = email.substring(4, 10);
+
+// let result = email.replace('m', 'w');
+
+// Replaces the first occurence
+let result = email.replace('n', 'w');
+
+console.log(result);
